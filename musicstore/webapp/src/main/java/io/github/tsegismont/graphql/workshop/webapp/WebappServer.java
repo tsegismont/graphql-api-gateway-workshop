1e4bbd63eab17b10423204c46a3657c7011ca9e2
Thomas Segismont <tsegismo@redhat.com>
2019-10-11 17:30:57 +0200

    Initial version of musicstore schema

--- musicstore/webapp/src/main/java/io/github/tsegismont/graphql/workshop/webapp/WebappServer.java
+++ musicstore/webapp/src/main/java/io/github/tsegismont/graphql/workshop/webapp/WebappServer.java
@@ -1,10 +1,20 @@
 package io.github.tsegismont.graphql.workshop.webapp;
 
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.idl.RuntimeWiring;
+import graphql.schema.idl.SchemaGenerator;
+import graphql.schema.idl.SchemaParser;
+import graphql.schema.idl.TypeDefinitionRegistry;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.ext.web.handler.ErrorHandler;
 import io.vertx.ext.web.handler.StaticHandler;
+import io.vertx.ext.web.handler.graphql.GraphQLHandler;
+import io.vertx.ext.web.handler.graphql.GraphQLHandlerOptions;
+import io.vertx.ext.web.handler.graphql.GraphiQLHandler;
+import io.vertx.ext.web.handler.graphql.GraphiQLHandlerOptions;
 
 public class WebappServer extends AbstractVerticle {
 
@@ -15,6 +25,9 @@ public class WebappServer extends AbstractVerticle {
 
     router.route().handler(BodyHandler.create());
 
+    router.route("/graphql").handler(createGraphQLHandler());
+    router.get("/graphiql/*").handler(createGraphiQLHandler());
+
     router.get().handler(StaticHandler.create());
 
     router.route().failureHandler(ErrorHandler.create(true));
@@ -23,5 +36,33 @@ public class WebappServer extends AbstractVerticle {
       .requestHandler(router)
       .listen(8080);
 
+  }
+
+  private GraphiQLHandler createGraphiQLHandler() {
+    GraphiQLHandlerOptions options = new GraphiQLHandlerOptions()
+      .setEnabled(true);
+    return GraphiQLHandler.create(options);
+  }
+
+  private GraphQLHandler createGraphQLHandler() {
+    GraphQLHandlerOptions options = new GraphQLHandlerOptions()
+      .setRequestBatchingEnabled(true);
+    return GraphQLHandler.create(setupGraphQLJava(), options);
+  }
+
+  private GraphQL setupGraphQLJava() {
+    String schema = vertx.fileSystem().readFileBlocking("musicstore.graphql").toString();
+
+    SchemaParser schemaParser = new SchemaParser();
+    TypeDefinitionRegistry typeDefinitionRegistry = schemaParser.parse(schema);
+
+    RuntimeWiring runtimeWiring = RuntimeWiring.newRuntimeWiring()
+      .build();
+
+    SchemaGenerator schemaGenerator = new SchemaGenerator();
+    GraphQLSchema graphQLSchema = schemaGenerator.makeExecutableSchema(typeDefinitionRegistry, runtimeWiring);
+
+    return GraphQL.newGraphQL(graphQLSchema)
+      .build();
   }
 }
