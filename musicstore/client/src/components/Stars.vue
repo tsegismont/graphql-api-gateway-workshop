ddcefee1c19c24c2ffacfea00952f88d4abf4285
Thomas Segismont <tsegismo@redhat.com>
2019-10-17 23:38:45 +0200

    Added average rate in album card

---
+++ musicstore/client/src/components/Stars.vue
@@ -0,0 +1,27 @@
+<template>
+  <span>
+    {{rating}} / {{maxRating}}
+    <span v-if="roundedRating < 1">
+      <font-awesome-icon :icon="['far', 'star']"/>
+    </span>
+    <span v-if="roundedRating >= 1">
+      <font-awesome-icon :icon="['fas', 'star']" class="text-warning" v-for="n in count" :key="n"/>
+    </span>
+  </span>
+</template>
+
+<script>
+    export default {
+        name: "Stars",
+        data() {
+            return {
+                maxRating: 5,
+                roundedRating: Math.round(this.rating),
+                count: Math.min(this.maxRating, this.roundedRating)
+            }
+        },
+        props: {
+            rating: Number
+        }
+    }
+</script>
--- musicstore/client/src/main.js
+++ musicstore/client/src/main.js
@@ -4,6 +4,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {faStar as farStar} from '@fortawesome/free-regular-svg-icons'
+import {faStar as fasStar} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+library.add(farStar, fasStar);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false;
 
 new Vue({
