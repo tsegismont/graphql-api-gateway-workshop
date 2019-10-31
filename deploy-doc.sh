abf954b2e9da7c4535a22b486a551e4e4899f0fe
Thomas Segismont <tsegismo@redhat.com>
2019-10-31 16:09:11 +0100

    Run docker with current user id and group to be able to remove Asciidoctor work files

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -12,8 +12,9 @@ OUTPUT_FILE=index.html
 mkdir -p "${LOCAL_REPO}"
 git clone -b ${WEBSITE_BRANCH} ${REMOTE_REPO} "${LOCAL_REPO}"
 cp ${INPUT_FILE} "${WORKDIR}"
-docker run -v "${WORKDIR}":/documents/ -v "${LOCAL_REPO}":/output/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram /documents/${INPUT_FILE} -D /output -o ${OUTPUT_FILE}
+docker run -u $(id -u):$(id -g) -v "${WORKDIR}":/documents/ -v "${LOCAL_REPO}":/output/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram /documents/${INPUT_FILE} -D /output -o ${OUTPUT_FILE}
 cd "${LOCAL_REPO}"
+rm -rf .asciidoctor
 git add .
 git commit -m "Auto generated doc"
 git push origin ${WEBSITE_BRANCH}
