47b1dd663e3d15a6a60b90a933b519dda9c93b34
Thomas Segismont <tsegismo@redhat.com>
2019-11-03 18:41:46 +0100

    Added workshop step3

---
+++ steps/step-3/src/main/java/workshop/gateway/AlbumDataFetcher.java
@@ -0,0 +1,28 @@
+package workshop.gateway;
+
+import graphql.schema.DataFetchingEnvironment;
+import io.reactivex.Single;
+import workshop.model.Album;
+import workshop.model.RatingInfo;
+import workshop.repository.AlbumsRepository;
+import workshop.repository.RatingRepository;
+
+public class AlbumDataFetcher implements RxDataFetcher<Album> {
+
+  private final AlbumsRepository albumsRepository;
+  private final RatingRepository ratingRepository;
+
+  public AlbumDataFetcher(AlbumsRepository albumsRepository, RatingRepository ratingRepository) {
+    this.albumsRepository = albumsRepository;
+    this.ratingRepository = ratingRepository;
+  }
+
+  @Override
+  public Single<Album> rxGet(DataFetchingEnvironment env) {
+    Integer id = EnvironmentUtil.getIntegerArgument(env, "id");
+    // Find album by id
+    // Find rating info by id
+    // Use Single#zipWith operator for concurrent requests execution and merging results
+    return null;
+  }
+}
---
+++ steps/step-3/src/solution/java/workshop/gateway/AlbumsDataFetcher.java
@@ -0,0 +1,23 @@
+package workshop.gateway;
+
+import graphql.schema.DataFetchingEnvironment;
+import io.reactivex.Single;
+import workshop.model.Album;
+import workshop.repository.AlbumsRepository;
+
+import java.util.List;
+
+public class AlbumsDataFetcher implements RxDataFetcher<List<Album>> {
+
+  private final AlbumsRepository albumsRepository;
+
+  public AlbumsDataFetcher(AlbumsRepository albumsRepository) {
+    this.albumsRepository = albumsRepository;
+  }
+
+  @Override
+  public Single<List<Album>> rxGet(DataFetchingEnvironment env) {
+    Integer genre = EnvironmentUtil.getIntegerArgument(env, "genre");
+    return albumsRepository.findAll(genre);
+  }
+}
