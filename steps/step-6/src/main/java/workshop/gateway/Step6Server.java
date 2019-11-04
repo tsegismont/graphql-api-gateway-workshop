f965b1ab4ebbcdadf7d85777563e64b9ae78579c
Vladimir Dejanovic <ed.wong.iv@gmail.com>
2019-11-04 23:39:08 +0100

    Looks like this was mixed up with Step 7

--- steps/step-6/src/main/java/workshop/gateway/Step6Server.java
+++ steps/step-6/src/main/java/workshop/gateway/Step6Server.java
@@ -63,7 +63,7 @@ public class Step6Server extends WorkshopVerticle {
 
   private TypeRuntimeWiring.Builder mutation(TypeRuntimeWiring.Builder builder) {
     return builder
-      .dataFetcher("addReview", new AddReviewDataFetcher(ratingRepository))
+      // Add data fetcher for the addReview field of the Mutation type
       ;
   }
 
