5ff4d2465fb34333d7c296457f57a8c2ab9b4f3d
Thomas Segismont <tsegismo@redhat.com>
2019-11-05 12:01:33 +0100

    Fixed deploy-doc.sh to be able to read source files

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -5,15 +5,17 @@ set -e -x
 WORKDIR=$(mktemp -d)
 REMOTE_REPO=git@github.com:tsegismont/graphql-api-gateway-workshop.git
 WEBSITE_BRANCH=gh-pages
-LOCAL_REPO="${WORKDIR}/${WEBSITE_BRANCH}"
+LOCAL_MASTER_REPO="${WORKDIR}/master"
+LOCAL_PAGES_REPO="${WORKDIR}/${WEBSITE_BRANCH}"
 INPUT_FILE=workshop.adoc
 OUTPUT_FILE=index.html
 
-mkdir -p "${LOCAL_REPO}"
-git clone -b ${WEBSITE_BRANCH} ${REMOTE_REPO} "${LOCAL_REPO}"
-cp ${INPUT_FILE} "${WORKDIR}"
-docker run -u $(id -u):$(id -g) -v "${WORKDIR}":/documents/ -v "${LOCAL_REPO}":/output/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram /documents/${INPUT_FILE} -D /output -o ${OUTPUT_FILE}
-cd "${LOCAL_REPO}"
+mkdir -p "${LOCAL_MASTER_REPO}"
+git clone ${REMOTE_REPO} "${LOCAL_MASTER_REPO}"
+mkdir -p "${LOCAL_PAGES_REPO}"
+git clone -b ${WEBSITE_BRANCH} ${REMOTE_REPO} "${LOCAL_PAGES_REPO}"
+docker run -u $(id -u):$(id -g) -v "${LOCAL_MASTER_REPO}":/documents/ -v "${LOCAL_PAGES_REPO}":/output/ asciidoctor/docker-asciidoctor asciidoctor -r asciidoctor-diagram /documents/${INPUT_FILE} -D /output -o ${OUTPUT_FILE}
+cd "${LOCAL_PAGES_REPO}"
 rm -rf .asciidoctor
 git add .
 git commit -m "Auto generated doc"
